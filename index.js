ee71c5fa27749e88ee37fba9b38668f2712aed47
miShelbyT <mi.shelbyrose@gmail.com>
2020-11-08 21:00:54 -0500

    lab done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+console.log ('please work')
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+	let musiciansPlay = []
+	for (let i = 0; i < musicians.length; i++) {
+		{ 
+			musiciansPlay.push(`${musicians[i]} plays ${instruments[i]}`)
+		}
+	}
+	return musiciansPlay
+}
+
+function johnLennonFacts(facts) {
+	let updatedFacts = []
+	let i = 0
+	while (i < facts.length) {
+		updatedFacts.push(facts[i].concat("!!!"))
+		i++
+	}
+	return updatedFacts
+}
+
+function iLoveTheBeatles(num) {
+	let beatlesLove = []
+	
+	do { beatlesLove.push('I love the Beatles!')
+		num += 1
+	} while (num < 15)
+
+	return beatlesLove
+}
